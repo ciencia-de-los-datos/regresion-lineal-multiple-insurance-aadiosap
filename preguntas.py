607e94bb812738c382f3d8d0f594f6d2584a79d7
aadiosap <aadiosap@unal.edu.co>
2022-05-24 05:52:02 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -116,7 +116,7 @@ def pregunta_03():
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        'selectkBest__k': list(range(1,12)),
+        'selectKBest__k': list(range(1,12)),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
